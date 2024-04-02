d326032fbc0ce7d7420f3c37731d9bdd0e60bc67
giorgi diasamdize <gio.diasa@gmail.com>
2024-04-02 17:08:00 +0400

    added mvc

---
+++ HotelProject.Web/Controllers/ManagersController.cs
@@ -0,0 +1,24 @@
+﻿using HotelProject.Models;
+using HotelProject.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Controllers
+{
+    public class ManagersController : Controller
+    {
+        private readonly ManagerRepository _managerRepository;
+        private readonly HotelRepository _hotelRepository;
+        public ManagersController()
+        {
+            _managerRepository = new ManagerRepository();
+            _hotelRepository = new HotelRepository();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _managerRepository.GetManagers();
+            var hotels = await _hotelRepository.GetHotels();
+            ViewBag.HotelNames = hotels.ToDictionary(h => h.Id, h => h.Name);
+            return View(result);
+        }
+    }
+}
---
+++ HotelProject.Web/Controllers/RoomsController.cs
@@ -0,0 +1,23 @@
+﻿using HotelProject.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.Web.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly RoomRepository _roomRepository;
+        private readonly HotelRepository _hotelRepository;
+        public RoomsController()
+        {
+            _roomRepository = new RoomRepository();
+            _hotelRepository = new HotelRepository();
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _roomRepository.GetRooms();
+            var hotels = await _hotelRepository.GetHotels();
+            ViewBag.HotelNames = hotels.ToDictionary(h => h.Id, h => h.Name);
+            return View(result);
+        }
+    }
+}
